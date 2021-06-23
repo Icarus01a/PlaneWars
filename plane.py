c31eddbf552fcce50ae0591f9644f7f9a6ffde3d
LiliMing <1606467568@qq.com>
2021-06-23 18:22:39 +0800

    绘制飞机 炸弹 生命图标

--- plane.py
+++ plane.py
@@ -1,9 +1,32 @@
-# plane01.py   框架，部分初始化代码
+# plane02.py   框架，部分初始化代码
 import pygame
 import sys
 import traceback
+import myplane
 
 from pygame.locals import *
+
+def draw_score_bombs_lifes():
+    # 绘制全屏炸弹数量
+    bomb_text = bomb_font.render("X %d" % bomb_num, True, WHITE )
+    text_rect = bomb_text.get_rect()
+    screen.blit(bomb_image, (10,height - 10 - bomb_rect.height))
+    screen.blit(bomb_text, (20 + bomb_rect.width, height - 5 - text_rect.height))
+
+    #绘制剩余生命数量
+    if life_num:
+        for i in range(life_num):
+            screen.blit(life_image, \
+                        (width - 10 - (i + 1) * life_rect.width, \
+                        height - 10 - life_rect.height))
+
+    #绘制得分
+    score_text = score_font.render("Score : %s" % str(score), True, WHITE)
+    screen.blit(score_text, (10,5))
+
+def draw_me():
+    #绘制我方飞机
+    screen.blit(me.image1, me.rect)
 
 pygame.init()
 pygame.mixer.init()
@@ -24,9 +47,33 @@ pygame.mixer.music.load("sound/game_music.ogg")
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
 
+# 统计得分
+score = 0
+score_font = pygame.font.Font("font/font.ttf", 36)
+
+# 标志是否暂停游戏
+paused = False
+
+# 标志是否暂停游戏
+level = 1
+
+# 全屏炸弹
+bomb_image = pygame.image.load("images/bomb.png").convert_alpha()
+bomb_rect = bomb_image.get_rect()
+bomb_font = pygame.font.Font("font/font.ttf", 48)
+bomb_num = 3
+
+#生命数量
+life_image = pygame.image.load("images/life.png").convert_alpha()
+life_rect = life_image.get_rect()
+life_num = 3
+
+#生成我方飞机
+me = myplane.MyPlane(bg_size)
+
+clock = pygame.time.Clock()
 
 def main():
-    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -34,6 +81,9 @@ def main():
                 sys.exit()
 
         screen.blit(background, (0, 0))
+
+        draw_score_bombs_lifes()
+        draw_me()
 
         pygame.display.flip()
         clock.tick(60)
@@ -47,4 +97,3 @@ if __name__ == '__main__':
     except:
         traceback.print_exc()
         pygame.quit()
-        input()
