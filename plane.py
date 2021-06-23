e8a4453ebc447141b1a0c50c9253378f6d922e8b
LiliMing <1606467568@qq.com>
2021-06-23 19:07:18 +0800

    增加己方飞机控制 发射子弹

--- plane.py
+++ plane.py
@@ -1,7 +1,9 @@
-# plane01.py   框架，部分初始化代码
+# plane03.py   增加己方飞机控制发射子弹
 import pygame
 import sys
 import traceback
+import myplane
+import bullet
 
 from pygame.locals import *
 
@@ -23,10 +25,56 @@ RED = (255, 0, 0)
 pygame.mixer.music.load("sound/game_music.ogg")
 pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
+bullet_sound = pygame.mixer.Sound("sound/bullet.wav")
+bullet_sound.set_volume(0.2)
+bomb_sound = pygame.mixer.Sound("sound/use_bomb.wav")
+bomb_sound.set_volume(0.2)
+supply_sound = pygame.mixer.Sound("sound/supply.wav")
+supply_sound.set_volume(0.2)
+get_bomb_sound = pygame.mixer.Sound("sound/get_bomb.wav")
+get_bomb_sound.set_volume(0.2)
+get_bullet_sound = pygame.mixer.Sound("sound/get_bullet.wav")
+get_bullet_sound.set_volume(0.2)
+upgrade_sound = pygame.mixer.Sound("sound/upgrade.wav")
+upgrade_sound.set_volume(0.2)
+enemy3_fly_sound = pygame.mixer.Sound("sound/enemy3_flying.wav")
+enemy3_fly_sound.set_volume(0.2)
+enemy1_down_sound = pygame.mixer.Sound("sound/enemy1_down.wav")
+enemy1_down_sound.set_volume(0.2)
+enemy2_down_sound = pygame.mixer.Sound("sound/enemy2_down.wav")
+enemy2_down_sound.set_volume(0.2)
+enemy3_down_sound = pygame.mixer.Sound("sound/enemy3_down.wav")
+enemy3_down_sound.set_volume(0.5)
+me_down_sound = pygame.mixer.Sound("sound/me_down.wav")
+me_down_sound.set_volume(0.2)
 
+
+# 生成普通子弹
+bullet1 = []
+bullet1_index = 0
+BULLET1_NUM = 4
+for i in range(BULLET1_NUM):
+    bullet1.append(bullet.Bullet1(me.rect.midtop))
+
+# 生成超级子弹
+bullet2 = []
+bullet2_index = 0
+BULLET2_NUM = 8
+for i in range(BULLET2_NUM // 2):
+    bullet2.append(bullet.Bullet2((me.rect.centerx - 33, me.rect.centery)))
+    bullet2.append(bullet.Bullet2((me.rect.centerx + 30, me.rect.centery)))
+    bullet2.append(bullet.Bullet2((me.rect.centerx - 1, me.rect.centery)))
+
+#  用于延迟
+delay = 100
+
+#标志是否使用超级子弹
+is_double_bullet = True
+is_Triple_Tap = True
+
+clock = pygame.time.Clock()
 
 def main():
-    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -34,6 +82,40 @@ def main():
                 sys.exit()
 
         screen.blit(background, (0, 0))
+
+        if life_num and not paused:
+            # 检测用户的键盘操作
+            key_pressed = pygame.key.get_pressed()
+
+            if key_pressed[K_w] or key_pressed[K_UP]:
+                me.moveUp()
+            if key_pressed[K_s] or key_pressed[K_DOWN]:
+                me.moveDown()
+            if key_pressed[K_a] or key_pressed[K_LEFT]:
+                me.moveLeft()
+            if key_pressed[K_d] or key_pressed[K_RIGHT]:
+                me.moveRight()
+
+             # 发射子弹
+            if not (delay % 10):
+                bullet_sound.play()
+                if is_double_bullet:
+                    bullets = bullet2
+                    bullets[bullet2_index].reset((me.rect.centerx - 33, me.rect.centery))
+                    bullets[bullet2_index + 1].reset((me.rect.centerx + 30, me.rect.centery))
+                    bullet2_index = (bullet2_index + 2) % BULLET2_NUM
+                else:
+                    bullets = bullet1
+                    bullets[bullet1_index].reset(me.rect.midtop)
+                    bullet1_index = (bullet1_index + 1) % BULLET1_NUM
+
+            # 检测子弹是否击中敌机
+            for b in bullets:
+                b.move()
+                screen.blit(b.image, b.rect)
+
+        delay = (delay - 1) if delay else 100
+
 
         pygame.display.flip()
         clock.tick(60)
@@ -47,4 +129,3 @@ if __name__ == '__main__':
     except:
         traceback.print_exc()
         pygame.quit()
-        input()
